032caddefe5e70589af7a878d7bd616edb205ff1
M2020sal2 <161150074+Elsamanoudy74@users.noreply.github.com>
2024-03-06 03:57:30 +0200

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import { bootstrap } from "./src/index.routes.js";
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+//bootstrap
+bootstrap(app, express);
+const port = parseInt(process.env.PORT);
+app.listen(port || 5000, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
+console.log("hi");
